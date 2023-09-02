bd2da3585eedfb57c2e71887606a24cd84dc5e3f
Mrityunjoy99 <mrityunjoydey1999@gmail.com>
2023-09-02 22:36:33 +0530

    feat(init): First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'onewaydatabinding';
-}
+  name='enter name';
+  address='enter address';
+  id='enter id';
+
+
+  itemImageUrl='../assets/Image20230901165727.jpg';
+  itemImage1='../assets/Image20230901165821.jpg';
+  isUnchanged=false;
+  isunchanged1=false;
+  width='300px';
+  height='300px';
+  value='red';
+  clsName = 'c1';
+  callFunction(){
+    console.log("this is event binding in angular...");
+    console.log("welcome");
+  }
+
+    call1(){
+
+ console.log("this is button test");
+ console.log("welcome1");
+
+    }
+
+    user={
+
+     userid:'13',
+     username:'debu'
+
+
+    };
+
+    hrefval='https://www.pcsglobal.in'
+  }
+
